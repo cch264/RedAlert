2c0b8b8def07271515a9568935c8457be881a982
Nick Nannen <nnannen1129@outlook.com>
2021-12-07 18:33:00 -0700

    Added email settings for sending notifications
    Added in the settings to configure our email account to send emails in Django.

--- RedAlert/RedAlert/settings.py
+++ RedAlert/RedAlert/settings.py
@@ -134,3 +134,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email Settings
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'RedAlertTester@gmail.com'
+EMAIL_HOST_PASSWORD = 'RedAlertcs476'
+EMAIL_USE_TLS = True
