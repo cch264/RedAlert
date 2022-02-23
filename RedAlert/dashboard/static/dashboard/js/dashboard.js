31ad15d269b3f25c74633c388b8d1746672aa120
Calvin Harper <cch264@nau.edu>
2022-02-22 18:34:21 -0700

    added comments

--- RedAlert/dashboard/static/dashboard/js/dashboard.js
+++ RedAlert/dashboard/static/dashboard/js/dashboard.js
@@ -161,8 +161,11 @@ function executeFuseSearch( user_pattern )
 
       //console.log(`JSON  ${ JSON.parse( $('#client-json-input').val()) }`);
 
+      // Get the json from the input element and parse it to convert it to a json.
+      // This json has an entry for EVERY single client in the DB.
       search_data_json = JSON.parse( $('#client-json-input').val());
 
+      // pass the client json to the search function.
       const fuse = new Fuse( search_data_json, options);
       
       // Change the pattern
