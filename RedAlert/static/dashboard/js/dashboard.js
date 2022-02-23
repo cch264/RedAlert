3e07c0e0c13e12278706748befc748d1054768eb
Calvin Harper <cch264@nau.edu>
2022-02-22 17:33:32 -0700

    Moved script tags around

--- RedAlert/static/dashboard/js/dashboard.js
+++ RedAlert/static/dashboard/js/dashboard.js
@@ -1,6 +1,5 @@
-const Fuse = require('fuse.js')
 
-[
+list = [
     {
       "title": "The DaVinci Code",
       "author": {
@@ -141,17 +140,35 @@ function executeFuseSearch( user_pattern )
 
         // Specifies keys in your json to search by.
         keys: [
-          "title",
-          "author.firstName"
+          "id",
+          "name",
+          "unit_num",
+          "street",
+          "city",
+          "zip_code",
+          "state",
+          "license_num",
+          "policies",
+          "age",
+          "birthdate",
+          "gender",
+          "notificatoin_status",
+          "email"
         ]
       };
-      
-      const fuse = new Fuse(list, options);
+    
+      //console.log(`JSON STRING ${ $('#client-json-input').val() }`);
+
+      //console.log(`JSON  ${ JSON.parse( $('#client-json-input').val()) }`);
+
+      search_data_json = JSON.parse( $('#client-json-input').val());
+
+      const fuse = new Fuse( search_data_json, options);
       
       // Change the pattern
       const pattern = ""
 
-      console.log( fuse.search( user_pattern ) )
+      //console.log( fuse.search( user_pattern ) )
       
       return fuse.search( user_pattern )
 }
@@ -162,14 +179,111 @@ window.addEventListener('load', (event) => {
     // Overwrite the forms on submit method so we can add our custom ajax function to execute on form submit.
     $('#user-search-box-form').on('submit', function(e)
         {
-            // PREVENT the default behavior of the form. Without this line, ajax wont work right.
+            // PREVENT the default behavior of the form. Without this line, ajax wont work right or FUSE.js.
             e.preventDefault();
 
             //executeSearchAjax();
 
-            executeFuseSearch(  $('#user-search-input').val() );
+            search_result_object = executeFuseSearch(  $('#user-search-input').val() );
+
+            console.log(`Search result leng ${search_result_object.length }`);
+
+            fill_client_results_box( search_result_object );
         })
   });
+
+
+function fill_client_results_box( client_list )
+{
+  // Remove previous search results
+  $('#search-results-container').empty();
+
+
+  client_list.forEach( ( result ) => {
+
+
+
+  let element_to_append = `
+    <div id="sr-${result.item.id}" data-client-id="${result.item.id}" class="bootstrap-grey-bottom my-1 me-1 ms-5 d-flex flex-row client-sr">
+                <input type="checkbox" class="me-5 ms-3" >
+                  
+                <div class="container">
+
+                  <div class="row mb-1">
+                    <div class="col"> Name: ${result.item.name} </div>
+                    <div class="col"> Address: ${result.item.unit_num} ${result.item.street} ${result.item.city}, ${result.item.state}, ${result.item.zip_code}</div> 
+                    <div class="col"> Policies: ${result.item.polcies}</div>
+                  </div>
+
+                  <div class="row mb-1">
+                    <div class="col"> Age: ${result.item.age}</div>
+                    <div class="col"> Birthdate: ${result.item.birthdate}</div>
+                    <div class="col"> Gender: ${result.item.gender}</div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col"> Notification Status: ${result.item.notification_status}</div>
+                    <div class="col"> License Number: ${result.item.license_num}</div>
+                    <div class="col"> Phone Number: ${result.item.phone}</div>
+                  </div>
+
+                </div>
+    </div>
+    `;
+
+    /*
+  let element_to_append = 
+            "<div class=\"client-sr\"  data-client-id=\"" + result.item.id + "\" class=\"bootstrap-grey-bottom my-1 me-1 ms-5\">
+              <input type=\"checkbox\" class=\"me-5 ms-3\" >
+                 <span class=\"mx-5\"> Name: " + result.item.name +
+                "</span> <span class=\"mx-5\"> Address:"+ result.item.unit_num+ " " + result.item.street +" " + result.item.city  
+                    + ", " + result.item.state+", " +result.item.zip_code +"</span> 
+                <span class=\"mx-5\"> Policies: " + result.item.polcies +"</span></div>";
+    */
+
+    $('#search-results-container').append( element_to_append );
+
+    // Get the div that contains the search result.
+    $(`#sr-${result.item.id}`).on('click', (event) => {
+
+      
+
+      if( $(`#selected-sr-${result.item.id}`).length )
+      {
+        $(`#selected-sr-${result.item.id}`).remove();
+      }
+      else
+      {
+        $("#selected-clients-container").append( $(`#sr-${result.item.id}`).clone().attr('id', `selected-sr-${result.item.id}` ) );
+        // Remove all ids from the container
+        $("#selected-clients-id-array").val("");
+
+        let clientIdString = "";
+
+        $("#selected-clients-container").children().each( function(index) {
+          // Ignore index 0 as it is the input we are storing client ids in so it is not needed
+          if( index > 0)
+          {
+            console.log(`ID OF CHILD ${ $(this).attr('id')}`);
+  
+            clientIdString += $( this ).data("clientId") + " ";
+          }
+        })
+
+        $("#selected-clients-id-array").val(clientIdString);
+        
+      }
+
+    })
+
+    console.log(`The result: ${JSON.stringify(result.item) }`);
+    //console.log(`id ${ result.item.id }`);
+    //console.log(`name ${ result.item.name }`);
+    //console.log(`email ${ result.item.email }`);
+    //console.log(`age ${ result.item.age }`);
+
+});
+}
 
 function executeSearchAjax() {
     $.ajax({
