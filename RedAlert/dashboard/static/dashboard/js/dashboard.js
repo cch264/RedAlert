164d51ea93192791ce50480855f1c3bdf0fcef0b
Calvin Harper <cch264@nau.edu>
2022-02-27 12:03:43 -0700

    Fixed footer, expandable search box, clear filters button, filter display banner

--- RedAlert/dashboard/static/dashboard/js/dashboard.js
+++ RedAlert/dashboard/static/dashboard/js/dashboard.js
@@ -65,7 +65,7 @@ function executeFuseSearch( user_pattern, showAllResults=false )
       // We show all results on page load, and when the user selects new filters.
       if( showAllResults)
       {
-        console.log("Showing all");
+        //console.log("Showing all search results");
         unfiltered_search_results = fuse.search(" ");
   
         filtered_search = filterSearchResults(unfiltered_search_results);
@@ -104,7 +104,7 @@ function filterSearchResults( searchResultJSON )
         // Get key name from array.
         let keyName = innerArray[0];
 
-        console.log(`Filtering: Key name is: ${keyName}`)
+        //console.log(`Filtering: Key name is: ${keyName}`)
 
         // Filter our results json.
         searchResultJSON = searchResultJSON.filter( result =>
@@ -115,14 +115,14 @@ function filterSearchResults( searchResultJSON )
             {
               let pattern = innerArray[ innerIndex ];
 
-              console.log(`Matching pattern: ${pattern} against result city name ${result.item[keyName]}`)
+              //console.log(`Matching pattern: ${pattern} against result city name ${result.item[keyName]}`)
 
               // Create array of booleans, if the result contains the pattern, push true to the array.
               // If the filters are city: mesa, chandler then we need to check each search result for mesa OR chandler so we create an array that checks .
               matchesFilter.push( result.item[keyName].toLowerCase().includes(pattern).toString() );
             }
 
-            console.log(`matches filter arrary ${matchesFilter}`);
+            //console.log(`matches filter arrary ${matchesFilter}`);
 
             return matchesFilter.includes('true');
           })
@@ -179,7 +179,13 @@ function filterSearchResults( searchResultJSON )
 window.addEventListener('load', (event) => {
     console.log('page is fully loaded');
 
+    clearFilterInputs();
+
+    // When the page loads show all search results.
     executeSearch(true);
+    
+
+    $("#expand-sr-btn").on('click', addListenerToSearchResultScrollBox );
 
     // Overwrite the forms on submit method so we can add our custom ajax function to execute on form submit.
     $('#user-search-box-form').keyup(function(event)
@@ -200,6 +206,7 @@ window.addEventListener('load', (event) => {
         // Filters that take a range are assigned listeners here.
         assignSearchFilterRangeListeners();
 
+        $('#clear-filters-btn').on('click', clearFilterInputs );
 
   });
 
@@ -208,74 +215,91 @@ window.addEventListener('load', (event) => {
 
     $("[id$='-filter-pattern']").each( function()
     {
-
       //console.log(`Data is a range: ${ $(this).data('isRange') }`);
 
-      $(this).on('click',function()
-      {
-
-        // Check if user selected or deselected the filter
-        if( $(this).is(":checked") )
-        {
-           // if they selected it, add the search key to the array if it does not exist already, and add the search key within that array.
-          // If the filter key array has first element set to false, set it to true, else do nothing.
-          updateFilterArray( addFilter=true, $(this).data('queryKey'), $(this).data('queryPattern'));
-
-          // Execute a search for all clients but filter the results according to the filter.
-          executeSearch(true);
-
-        }
-        else
-        {
-          // If user deselected the filter, remove the filter pattern from the array. Check if any other filters are selected based on selected inputs on the page, do not use array to confirm this.
-          updateFilterArray( addFilter=false, $(this).data('queryKey'), $(this).data('queryPattern'));
-
-          // Execute a search for all clients but filter the results according to the filter that was removed.
-          executeSearch(true);
-
-        }
-      });
+      $(this).on('click', filterPatternClickEvent);
     });
 
   }
 
+  // this object auto passed by the .on method.
+  function filterPatternClickEvent()
+  {
+    
+      // Check if user selected or deselected the filter
+      if( $(this).is(":checked") )
+      {
+        //console.log("is Checked");
+         // if they selected it, add the search key to the array if it does not exist already, and add the search key within that array.
+        // If the filter key array has first element set to false, set it to true, else do nothing.
+        updateFilterArray( addFilter=true, $(this).data('queryKey'), $(this).data('queryPattern'));
+
+        updateAppliedFiltersDisplay();
+
+        // Execute a search for all clients but filter the results according to the filter.
+        executeSearch(true);
+
+      }
+      else
+      {
+        //console.log("is NOT Checked");
+        // If user deselected the filter, remove the filter pattern from the array. Check if any other filters are selected based on selected inputs on the page, do not use array to confirm this.
+        updateFilterArray( addFilter=false, $(this).data('queryKey'), $(this).data('queryPattern'));
+        
+        updateAppliedFiltersDisplay();
+
+        // Execute a search for all clients but filter the results according to the filter that was removed.
+        executeSearch(true);
+
+      }
+  }
+
   function assignSearchFilterRangeListeners()
   {
-     // When user lifts up key check inputs.
-     $( '#age-filter-range-pattern-1,#age-filter-range-pattern-2' ).keyup( function( event )
-     {
-       //console.log(`range value ${event.target.value}`);
- 
-       // Check the two range elements to see if they match up
-       let rangeIsGood = checkRange( $( '#age-filter-range-pattern-1' ), $( '#age-filter-range-pattern-2' ));
- 
-       if( rangeIsGood === 1 )
-       {
-         searchKeysAndRangePatterns[0] = true;
+     // When user lifts up key check inputs. This jquery selects two element each with the specified ids.
+     $( '#age-filter-range-pattern-1, #age-filter-range-pattern-2' ).keyup( searchFilterRangeClickEvent );
 
-         let rangeToPush = $('#age-filter-range-pattern-1').val() + '-' + $('#age-filter-range-pattern-2').val();
+  }
 
-         searchKeysAndRangePatterns[1] = ['age', rangeToPush ];
+  function searchFilterRangeClickEvent()
+  {
+      //console.log(`range value ${event.target.value}`);
 
-         // Execute a search for all clients but filter the results according to the filter removed.
-         executeSearch(true);
+      // Check the two range elements to see if they match up
+      let rangeIsGood = checkRange( $( '#age-filter-range-pattern-1' ), $( '#age-filter-range-pattern-2' ));
 
-         //console.log(`New range key array is ${searchKeysAndRangePatterns}`);
- 
-       }
-       // If user clears array or enters bad input, clear the array and execute a search again since our filter have changed.
-       // If user clears age range or enters bad range, remove the age range filter and rexecute search.
-       else if( rangeIsGood === 3 || rangeIsGood === 0)
-       {
-         searchKeysAndRangePatterns[0] = false;
-         searchKeysAndRangePatterns[1] = ['age'];
+      if( rangeIsGood === 1 )
+      {
+        searchKeysAndRangePatterns[0] = true;
 
-         //console.log(`New range key array is ${searchKeysAndRangePatterns}`);
-         // Execute a search for all clients but filter the results according to the filter removed.
-         executeSearch(true);
-       }
-     })
+        let rangeToPush = $('#age-filter-range-pattern-1').val() + '-' + $('#age-filter-range-pattern-2').val();
 
+        searchKeysAndRangePatterns[1] = ['age', rangeToPush ];
+
+        // Execute a search for all clients but filter the results according to the filter removed.
+        executeSearch(true);
+        
+        // Update the filter display since the user entered a new range
+        updateAppliedFiltersDisplay();
+
+
+        //console.log(`New range key array is ${searchKeysAndRangePatterns}`);
+
+      }
+      // If user clears array or enters bad input, clear the array and execute a search again since our filter have changed.
+      // If user clears age range or enters bad range, remove the age range filter and rexecute search.
+      else if( rangeIsGood === 3 || rangeIsGood === 0)
+      {
+        searchKeysAndRangePatterns[0] = false;
+        searchKeysAndRangePatterns[1] = ['age'];
+
+        //console.log(`New range key array is ${searchKeysAndRangePatterns}`);
+        // Execute a search for all clients but filter the results according to the filter removed.
+        executeSearch(true);
+
+        // Update the filter display since the user entered a new range
+        updateAppliedFiltersDisplay();
+      }
   }
 
 
@@ -350,13 +374,13 @@ window.addEventListener('load', (event) => {
         // each inner array represents a search key and pattersn to match. ie city could be search key and pattersn to match would be 'mesa' or 'phoenix'
         searchKeyInnerArray = searchKeysAndPatterns[filterIndex];
 
-        console.log(`Search key inner array: ${searchKeyInnerArray}`);
+        //console.log(`Search key inner array: ${searchKeyInnerArray}`);
 
         if( searchKeyInnerArray[0] === queryKey )
         {
           searchKeyInnerArray.push(queryPattern);
 
-          console.log(`Filter array after adding new filter: ${searchKeysAndPatterns}`);
+          //console.log(`Filter array after adding new filter: ${searchKeysAndPatterns}`);
 
           // We added the filter, break out of the function.
           return;
@@ -374,7 +398,7 @@ window.addEventListener('load', (event) => {
         // each inner array represents a search key and pattersn to match. ie city could be search key and pattersn to match would be 'mesa' or 'phoenix'
         searchKeyInnerArray = searchKeysAndPatterns[filterIndex];
 
-        console.log(`Inner array when removing filter: ${searchKeyInnerArray}`);
+        //console.log(`Inner array when removing filter: ${searchKeyInnerArray}`);
 
         if( searchKeyInnerArray[0] === queryKey )
         {
@@ -384,7 +408,7 @@ window.addEventListener('load', (event) => {
               }
             );
 
-          console.log(`Inner array after remove filter ${searchKeysAndPatterns[filterIndex] }`);
+          //console.log(`Inner array after remove filter ${searchKeysAndPatterns[filterIndex] }`);
 
           // Break out of the loop. A while loop would have been more appropraite here.
           filterIndex = searchKeysAndPatterns.length;
@@ -392,7 +416,7 @@ window.addEventListener('load', (event) => {
       }
 
 
-      console.log(`Filter array before checking for no filters: ${searchKeysAndPatterns}`);
+      //console.log(`Filter array before checking for no filters: ${searchKeysAndPatterns}`);
       // After removing the element from the search pattern array, we need to check if the array is empty and if we should not apply any filters
       // If there are not selected filters set the first element of searchKeysAndPatterns to false else break the loop and do nothing.
       // Start 1 because index 0 just holds a boolean
@@ -401,13 +425,13 @@ window.addEventListener('load', (event) => {
         // each inner array represents a search key and pattersn to match. ie city could be search key and pattersn to match would be 'mesa' or 'phoenix'
         searchKeyInnerArray = searchKeysAndPatterns[filterIndex];
 
-        console.log(`Search key inner array: ${searchKeyInnerArray}`);
+        //console.log(`Search key inner array: ${searchKeyInnerArray}`);
 
         // If one of the search key inner arrays has length greater than 1, that means there are still selected filters on the page.
         // Break loop were done.
         if( searchKeyInnerArray.length > 1)
         {
-          console.log(`Filter array has filters still, leave first elem true: ${searchKeysAndPatterns}`);
+          //console.log(`Filter array has filters still, leave first elem true: ${searchKeysAndPatterns}`);
           return;
         }
       }
@@ -415,7 +439,7 @@ window.addEventListener('load', (event) => {
       // If we dont return that means there are not applied filters. Set the first elem of the searchKeysAndPatterns Array to false.
       searchKeysAndPatterns[0] = false;
 
-      console.log(`No filter selected. Setting first elem to false: ${searchKeysAndPatterns}`);
+      //console.log(`No filter selected. Setting first elem to false: ${searchKeysAndPatterns}`);
     }
 
   }
@@ -439,7 +463,95 @@ window.addEventListener('load', (event) => {
       searchKeysAndRangePatterns.push( [ $(this).text().toLowerCase() ] );
     })
 
-    console.log(`Search key range array is ${searchKeysAndRangePatterns}`);
+    //console.log(`Search key range array is ${searchKeysAndRangePatterns}`);
+  }
+
+  function updateAppliedFiltersDisplay()
+  {
+    let appliedFiltersStr = "Applied Filters: ";
+
+    // If there are filters selected, then index 0 will be true.
+    if( searchKeysAndPatterns[0] )
+    {
+
+      for( let index = 1 ; index < searchKeysAndPatterns.length; index++ )
+      {
+        let innerFilterArray = searchKeysAndPatterns[index]
+
+        // If the inner arrays length is == 1 then that means there are not filters applied for that category.
+        if( innerFilterArray.length > 1)
+        {
+          for( let innerIndex in innerFilterArray)
+          {
+            console.log(`Inner filter array ${innerFilterArray}`);
+            console.log(`Inner index ${innerIndex}`);
+  
+            if( innerIndex == 0)
+            {
+              appliedFiltersStr += "<strong>" + innerFilterArray[innerIndex].trim().replace(/^\w/, (c) => c.toUpperCase()) + "</strong>: ";
+            }
+            // Check if were at end of the array
+            else if( innerIndex != (innerFilterArray.length - 1) )
+            {
+              appliedFiltersStr += innerFilterArray[innerIndex].trim().replace(/^\w/, (c) => c.toUpperCase()) + ", ";
+            }
+            else
+            {
+              appliedFiltersStr += " " + innerFilterArray[innerIndex].trim().replace(/^\w/, (c) => c.toUpperCase()) + ". ";
+            }
+          }
+        }
+      }
+
+    }
+    if( searchKeysAndRangePatterns[0] )
+    {
+
+      // Get the age range filter inner array.
+      let innerFilterArray = searchKeysAndRangePatterns[1];
+
+      appliedFiltersStr += " <strong>Age: </strong>" + innerFilterArray[1] + ". ";
+
+    }
+    // If both filter arrays do not have filters in them, then no filters are selected.
+    if( !searchKeysAndRangePatterns[0] && !searchKeysAndPatterns[0])
+    {
+      appliedFiltersStr += "None"
+    }
+
+    $('#appl-filters-display').html( appliedFiltersStr );
+  }
+
+
+  // On refresh, browsers like fire fox auto  fill what was previously entered. This would be ok, expect
+  // our inputs work based on click events, when the page refreshes and auto selects the inputs,
+  // no click event is fired and the filters will not work.
+  function clearFilterInputs()
+  {
+
+    $("[id$='-filter-pattern']").each( function()
+    {
+      $(this).prop("checked", false);
+
+      updateFilterArray( addFilter=false, $(this).data('queryKey'), $(this).data('queryPattern'));
+
+
+    });
+
+
+    $( '#age-filter-range-pattern-1,#age-filter-range-pattern-2' ).each( function()
+     {
+      $(this).val("");
+      // Manually reset the range filter array
+      searchKeysAndRangePatterns[0] = false;
+      searchKeysAndRangePatterns[1] = ['age'];
+     });
+
+     updateAppliedFiltersDisplay();
+
+     // Execute a search for all clients but filter the results according to the filter that was removed.
+     executeSearch(true);
+
   }
 
   
@@ -447,10 +559,18 @@ window.addEventListener('load', (event) => {
 
 function executeSearch( showAllResults=false ) 
 {
-  console.log("Search keys is " + search_keys); 
+  //console.log("Search keys is " + search_keys); 
 
+  let userInput =  $('#user-search-input').val();
 
-  search_result_object = executeFuseSearch(  $('#user-search-input').val(), showAllResults );
+  if(userInput === "" )
+  {
+    search_result_object = executeFuseSearch(  $('#user-search-input').val(), true );
+  }
+  else
+  {
+    search_result_object = executeFuseSearch(  $('#user-search-input').val(), showAllResults );
+  }
 
   fill_client_results_box( search_result_object );
 
@@ -565,19 +685,19 @@ function generateSearchElement(result)
 
                 <div class="row mb-1">
                   <div class="col"> Name: ${result.item.name} </div>
-                  <div class="col"> Address: ${result.item.unit_num} ${result.item.street} ${result.item.city}, ${result.item.state}, ${result.item.zip_code}</div> 
+                  <div class="col-6"> Address: ${result.item.unit_num} ${result.item.street} ${result.item.city}, ${result.item.state}, ${result.item.zip_code}</div> 
                   <div class="col"> Policies: ${result.item.policies}</div>
                 </div>
 
                 <div class="row mb-1">
                   <div class="col"> Age: ${result.item.age}</div>
-                  <div class="col"> Birthdate: ${result.item.birthdate}</div>
+                  <div class="col-6"> Birthdate: ${result.item.birthdate}</div>
                   <div class="col"> Gender: ${result.item.gender}</div>
                 </div>
 
                 <div class="row">
                   <div class="col"> Notification Status: ${result.item.notification_status}</div>
-                  <div class="col"> License Number: ${result.item.license_num}</div>
+                  <div class="col-6"> License Number: ${result.item.license_num}</div>
                   <div class="col"> Phone Number: ${result.item.phone}</div>
                 </div>
 
@@ -627,6 +747,24 @@ function refreshSelectedClientsString()
 
 
   $("#selected-clients-id-array").val(clientIdString);
+}
+
+function addListenerToSearchResultScrollBox()
+{
+
+  // If search results container is scrollable, expand it.
+  if( $('#search-results-container').hasClass('sr-scroll-box') )
+  {
+    $('#search-results-container').removeClass('sr-scroll-box');
+
+    $('#expand-sr-btn').html("Minimize Search Results");
+  }
+  else // If search results container is expanded, minimize it.
+  {
+    $('#search-results-container').addClass('sr-scroll-box');
+    $('#expand-sr-btn').html("Expand Search Results");
+  }
+
 }
 
 
